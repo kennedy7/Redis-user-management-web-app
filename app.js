2636853459c6a45e7c2e31dceefabaa87ed65534
kennedy7 <kennedyhillary6@gmail.com>
2022-06-07 18:57:25 +0100

    initiall

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require ('express');
-const exphbs = require('express-handlebars');
 const ejs = require('ejs');
 const path = require ('path');
 const bodyParser = require ('body-parser');
